a3aff44665e87c66f7ce81377abdf1e918fefe7d
Robert Anderson <reanderson89@gmail.com>
2020-11-15 16:36:12 -0800

    figured out how to connect the the css and js to the html files on the server. Currently have the app running with no errors in the console. Notes added are not populating in the side panel.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const notesDB = require("./db/db.json");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+let rawNotes = fs.readFileSync("db/db.json");
+let notes = JSON.parse(rawNotes);
 
 // middleware to parse incoming body
 app.use(express.urlencoded({ extended: true}));
@@ -18,12 +23,23 @@ app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-// api routes
-app.get("/api/config", (req,res) => {
-    res.json({
-        success: true,
-    });
+app.get("/assets/js/index.js", (req,res) => {
+    res.sendFile(path.join(__dirname, "assets/js/index.js"));
 });
+
+app.get("/assets/css/styles.css", (req,res) => {
+    res.sendFile(path.join(__dirname, "/assets/css/styles.css"));
+});
+
+// api routes
+app.get("/api/notes", (req,res) => {
+    return res.json(notes);
+});
+
+app.post("/api/notes", (req,res) => {
+notesDB.push(req.body);
+return res.json(notesDB);
+})
 
 app.listen(PORT, function() {
     console.log(`App listening on server http://localhost:${PORT}`);

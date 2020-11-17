77464864ee092d5e14b7d8f90196267939a3a312
Robert Anderson <reanderson89@gmail.com>
2020-11-17 11:22:22 -0800

    found error that once all notes are deleted you cannot add another note.

--- server.js
+++ server.js
@@ -4,24 +4,16 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 let notesDB = require("./db/db.json");
-// const index = require("./assets/js/index");
 const app = express();
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const PORT = process.env.PORT || 8080;
-
-// let rawNotes = fs.readFileSync("./db/db.json");
-// let notes = JSON.parse(rawNotes);
 
 // middleware to parse incoming body
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // view routes
-// app.get("*", (req,res) => {
-//     res.sendFile(path.join(__dirname, "public/index.html"));
-// });
-
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
@@ -30,7 +22,7 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
+// routes that enable the server to access the .js and .css files
 app.get("/assets/js/index.js", (req,res) => {
     res.sendFile(path.join(__dirname, "/assets/js/index.js"));
 });
@@ -44,7 +36,9 @@ app.get("/api/notes", (req,res) => {
      res.json(notesDB);
 });
 
+// this takes in the users note, gives it an id, and then pushes the object into the notesDB array
 app.post("/api/notes", (req,res) => {
+    console.log('this posted');
 let note = req.body;
 let id = notesDB[notesDB.length-1].id+1;
 note.id = id;
@@ -56,9 +50,9 @@ writeFileAsync("./db/db.json", JSON.stringify(notesDB))
             .catch((err) => console.log(err));
 });
 
+// this creates a new array called 'notes2' after filtering through notesDB and only taking those that don't match the chosen id. Thus deleting the chosen id from the array
 app.delete("/api/notes/:id", (req,res) => {
     let notes2 = notesDB.filter(note => note.id != req.params.id);
-
 notesDB=notes2;
 writeFileAsync("./db/db.json", JSON.stringify(notesDB)).then(() => {
     res.json(notesDB);
